e6c2a4d091683cbbc1a09c4e1bd7b6d4e9135321
Tiago Salvatierra <tiagosalvatierra258@gmail.com>
2025-10-01 08:39:45 -0300

    feat: implement obtaining coordinates on click

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,43 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
+import { createBrowserRouter } from "react-router-dom";
+import "/src/styles.css";
 
-function App() {}
+// const router = createBrowserRouter([
+//   {
+//     path:"/",
+//     element:
+//   }
+// ])
+
+function App() {
+  const waldoX = 28;
+  const waldoY = 28;
+  const imageRef = useRef(null);
+
+  useEffect(() => {
+    const image = imageRef.current;
+    image.addEventListener("click", handleClick);
+  }, []);
+
+  const handleClick = (e) => {
+    const target = e.target;
+
+    const x = e.pageX - target.offsetLeft;
+    const y = e.pageY - target.offsetTop;
+
+    const xCoord = Math.floor((x / target.width) * 10000) / 100;
+    const yCoord = Math.floor((y / target.height) * 10000) / 100;
+
+    console.log(xCoord, yCoord);
+  };
+
+  return (
+    <img
+      ref={imageRef}
+      src="/public/67c3c32bfa2b9791019b63f40f0bcccd.webp"
+      className="image"
+    ></img>
+  );
+}
 
 export default App;
